8bdabf82a091e2f42dee64f1ed9ccc89cfdfbb34
Devilek66 <Aleksanderp1993@gmail.com>
2018-04-05 13:47:14 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Xml.Linq;
+using System.Xml.Schema;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlSchemaSet schemas = new XmlSchemaSet();
+            schemas.Add("", "../../library.xsd");
+
+            XDocument Doc = XDocument.Load("../../library.xml");
+            bool errors = false;
+            Doc.Validate(schemas, (o, e) =>
+            {
+                Console.WriteLine("{0}", e.Message);
+                errors = true;
+            });
+            Console.WriteLine("Valid? {0}", errors ? "Nope" : "Yep");
+
+            Console.ReadKey();
+        }
+        
+    }
+}
